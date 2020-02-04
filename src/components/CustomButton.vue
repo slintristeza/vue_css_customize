4b089818dcbaf883726b76dba4f5cb0c052930a4
slintristeza <slintristeza@gmail.com>
2020-02-04 20:11:49 +0900

    use scoped style

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,6 +1,12 @@
 <template>
-  <v-btn class="ma-2" tile color="indigo" dark style="width:300px">Custom Button</v-btn>
+  <v-btn class="ma-2 hoge" tile color="indigo" dark>Custom Button</v-btn>
 </template>
+
+<style scoped>
+  .hoge {
+    width: 300px;
+  }
+</style>
 
 <script>
 export default {
